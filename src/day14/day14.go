e461a95f2498a3eb215da13bf5c0dcc2fd203c19
CE88888888 <celbersen@hotmail.com>
2021-12-14 17:12:35 +0100

    day14 Exercise B

--- src/day14/day14.go
+++ src/day14/day14.go
@@ -9,7 +9,7 @@ import (
 
 type elements map[string]int
 
-func (e *elements) Count(poly []string) {
+func (e *elements) CountStringArray(poly []string) {
 	f := *e
 	for _, value := range poly {
 		f[value]++
@@ -26,39 +26,95 @@ func main() {
 	scanner.Scan()
 	poly := strings.Split(start, "")
 
+	//get mapping rules
 	rules := map[string]string{}
 	for scanner.Scan() {
 		line := scanner.Text()
 		rules[line[0:2]] = string(line[6])
 	}
 
-	eles := getElements(rules)
+	paircount := newPairCount(rules, poly)
+	eleB := getElements(rules)
 
-	for i := 0; i < 40; i++ {
+	//Exercise A
+	eleA := getElements(rules)
+	for i := 0; i < 10; i++ {
 		poly = growOnce(poly, rules)
 		print(i)
 	}
-	eles.Count(poly)
-	min, max := getLowMax(eles)
+	eleA.CountStringArray(poly)
+	min, max := getLowMax(eleA)
+	fmt.Println(max - min)
 
-	print(start, poly[0])
-	fmt.Println(min)
-	fmt.Println(max)
+	//Exercise B
+	for i := 0; i < 40; i++ {
+		paircount = growAPair(paircount, rules)
+	}
+	eleB = translatePairToSingle(paircount, eleB, rules)
+	minB, maxB := getLowMax(eleB)
+	fmt.Println(((maxB - minB) / 2) + 1)
+
+}
+
+func newPairCount(rules map[string]string, poly []string) map[string]int {
+	paircount := map[string]int{}
+	for key := range rules {
+		paircount[key] = 0
+	}
+
+	for i := 0; i+1 < len(poly); i++ {
+		pair := poly[i] + poly[i+1]
+		paircount[pair]++
+	}
+	return paircount
+}
+
+func translatePairToSingle(paircount map[string]int, eleB elements, rules map[string]string) elements {
+	eleR := eleB
+	for key, value := range paircount {
+		eleR[string(key[0])] += value
+		eleR[string(key[1])] += value
+	}
+	return eleR
+}
+
+func growAPair(pc map[string]int, rules map[string]string) map[string]int {
+	newpc := make(map[string]int, len(pc))
+	for k, v := range pc {
+		newpc[k] = v
+	}
+
+	for key, value := range pc {
+		if value > 0 {
+			np1 := string(key[0]) + rules[key]
+			np2 := rules[key] + string(key[1])
+
+			newpc[np1] += value
+			newpc[np2] += value
+			newpc[key] -= value
+		}
+	}
+	return newpc
 }
 
 func getLowMax(eles elements) (min int, max int) {
-	for _, value := range eles {
+	keyMin, keyMax := "", ""
+	for key, value := range eles {
+		keyMin = key
 		min = value
 		break
 	}
-	for _, value := range eles {
+	for key, value := range eles {
 		if value > max {
+			keyMax = key
 			max = value
 		}
 		if value < min {
+			keyMin = key
 			min = value
 		}
 	}
+	fmt.Println("Min:", keyMin, " Max:", keyMax)
 	return
 }
 
@@ -81,13 +137,11 @@ func growOnce(poly []string, rules map[string]string) (newPoly []string) {
 		newpair[0] = poly[i]
 		newpair[1] = rules[pair]
 
-		//newpair[2] = poly[i+1]
 		newPoly = append(newPoly, newpair...)
 		if i+2 == len(poly) {
 			newPoly = append(newPoly, poly[i+1])
 		}
 	}
-
 	return
 }
 
